a6614e2f965f17352d2f001a63b9e455b9a07def
emeath <matheus.martins.jeronimo@gmail.com>
2022-07-21 11:16:29 -0300

    Adds debug mode and gets tv show name, rating and poster

--- src/App.java
+++ src/App.java
@@ -16,12 +16,15 @@ public class App {
      */
     public static void main(String[] args) throws Exception {
 
-        //read api key from environment variable
+        boolean debug = false;
+
+        // read api key from environment variable
         Map<String, String> env = System.getenv();
         String apiKeyMatheus = env.get("APIKEYMATHEUS");
 
         // make HTTP conection and search 250 top tv shows
         String baseUrl = "https://api.themoviedb.org/3";
+        String imgBaseUrl = "https://image.tmdb.org/t/p/w500";
         String endpoint = "/discover/tv";
         String requiredParam = "?api_key=" + apiKeyMatheus;
         String optionalParam = "";
@@ -33,26 +36,20 @@ public class App {
         var request = HttpRequest.newBuilder(address).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        //System.out.println(body);
-        
-        JSONObject jsonObject = new JSONObject(body);
-        System.out.println(jsonObject.getJSONArray("results").getJSONObject(0).get("original_name"));
-        //System.out.println(jsonObject.get("poster_path"));
-        //System.out.println(jsonObject.get("vote_average"));
 
-        //titulo, imagem, classificacao
-        //System.out.println(jsonObject.get("title"));
+        if (debug) {
+            System.out.println(body);
+            System.exit(-1);
+        }
+
+        JSONObject jsonObject = new JSONObject(body);
+
+        for (int i = 0; i < jsonObject.getJSONArray("results").length(); i++) {
+            System.out.println(jsonObject.getJSONArray("results").getJSONObject(i).get("original_name"));
+            System.out.println(jsonObject.getJSONArray("results").getJSONObject(i).get("vote_average"));
+            System.out.println(imgBaseUrl + jsonObject.getJSONArray("results").getJSONObject(i).get("poster_path"));
+            System.out.println("---");
+        }
+
     }
 }
-
-
-//https://api.themoviedb.org/3/discover/movie?api_key=paodequeijo&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate
-
-/*
- * https://www.themoviedb.org/documentation/api
- * https://www.themoviedb.org/documentation/api/wrappers-libraries
- * https://www.themoviedb.org/settings/api
- * https://developers.themoviedb.org/3/getting-started/introduction
- * https://www.themoviedb.org/documentation/api
- * 
- */
