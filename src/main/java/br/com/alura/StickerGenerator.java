d35edcfd664d0c983437c91ca8594d8c443f6b76
emeath <matheus.martins.jeronimo@gmail.com>
2022-07-22 15:38:50 -0300

    Reads image from disk and writes centered legend

--- src/main/java/br/com/alura/StickerGenerator.java
+++ src/main/java/br/com/alura/StickerGenerator.java
@@ -1,5 +1,7 @@
 package br.com.alura;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -24,10 +26,22 @@ public class StickerGenerator {
 		Graphics2D graphics = (Graphics2D) newImage.getGraphics();
 		graphics.drawImage(originalImage, 0, 0, null);
 		
+		// configure font
+		Font font = new Font(Font.SANS_SERIF, Font.BOLD, 100);
+		graphics.setFont(font);
+		graphics.setColor(Color.YELLOW);
+		
+		// write phrase on new image
+		String stickerLegend = "CONFIA";
+		int stringWidth = graphics.getFontMetrics().stringWidth(stickerLegend);
+		int centeringPosition = Math.round((width - stringWidth)/2);
+		graphics.drawString(stickerLegend, centeringPosition, newHeight - 100);
+		
 		// write new image on file
 		File outputImageFile = new File("src/main/resources/output/movie-sticker.png");
 		// Create output dir if it doesn't exists
 		String createdStickersFolderPath = outputImageFile.getParent();
+		@SuppressWarnings("unused")
 		boolean mkdir = new File(createdStickersFolderPath).mkdir();
 		ImageIO.write(newImage, "png", outputImageFile);
 		
